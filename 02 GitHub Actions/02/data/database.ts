0b9b0d09a97f0f6f1fa99f527b59bcc6955c2168
Michal Radomski <michalra@op.pl>
2025-04-18 20:22:39 +0200

    GitHub Actions course - commit 28

--- 02 GitHub Actions/02/data/database.ts
+++ 02 GitHub Actions/02/data/database.ts
@@ -3,12 +3,14 @@ dotenv.config();
 
 import { MongoClient } from "mongodb";
 
+// const connectionProtocol = process.env.MONGODB_CONNECTION_PROTOCOL as string;
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS as string;
 const dbUser = process.env.MONGODB_USERNAME as string;
 const dbPassword = process.env.MONGODB_PASSWORD as string;
 const dbName = process.env.MONGODB_DB_NAME as string;
 
 const uri: string = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+// const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client: MongoClient = new MongoClient(uri);
 
 console.log("Trying to connect to db");
@@ -22,6 +24,7 @@ console.log("Trying to connect to db");
     console.log("Connection failed.");
     await client.close();
     console.log("Connection closed.");
+    process.exit(1);
   }
 })();
 
